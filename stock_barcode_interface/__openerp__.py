c0f96e17ddcd605be9917494577b1c7ede488fde
Valentin Thirion <vt@abakusitsolutions.eu>
2016-11-07 17:33:18 +0100

    [ADD] stock_barcode_interface - Added module to repo - Added a button to print delivery notes from lists - Reorganized module internal data - Reorganized buttons in barcode interface (for printing)

---
+++ stock_barcode_interface/__openerp__.py
@@ -0,0 +1,48 @@
+{
+   'name': "Stock barcode interface",
+    'version': '9.0.1.0.2',
+    'depends': ['stock', 'stock_delivery_note_report', 'product_brand', 'deliveries_labels'],
+    'author': "AbAKUS it-solutions SARL",
+    'website': "http://www.abakusitsolutions.eu",
+    'category': 'Stock',
+    'description': 
+    """
+Stock barcode interface
+This module is based on the interface extension (odoo 8) of the warehouse module.
+
+New delivery note report (under construction)
+
+New menu items:
+    - Incoming product:
+        - (1) scan a product and select quantity (with search by EAN13)
+        - (2) select incoming picking and transfer
+        - (3) select outgoing picking, reserve and print labels
+    - All Products (with search)
+    - Moves Products: only products of incoming and outgoing picking lists (with search)
+    - Incomings: incoming pickings (with search)
+    - Outgoings: outgoing pickings (with search)
+    - Suppliers: picking lists by supplier (with search)
+    - Customers: picking lists by customer (with search)
+
+Improvements to existing functionalities:
+    - new information to the stock picking interface:
+        - picking:
+            - Partner
+            - Creation Date
+            - Scheduled Date
+            - Source Document
+            - State
+        - stock move:
+            - Availability
+            - Status
+
+This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions, under the control of Valentin Thirion.
+    """,
+    'data': [
+        'views/stock.xml',
+        'views/stock_picking.xml',
+    ],
+    'qweb': [
+        'static/src/xml/picking_improvements.xml',
+    ],
+}
