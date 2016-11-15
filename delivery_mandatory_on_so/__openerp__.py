5271b5e60e78d9348c6d30429a41ea54ab96e27f
Valentin Thirion <vt@abakusitsolutions.eu>
2016-11-15 17:00:34 +0100

    [ADD] delivery_mandatory_on_so Set required on view and not on model

--- delivery_mandatory_on_so/__openerp__.py
+++ delivery_mandatory_on_so/__openerp__.py
@@ -1,6 +1,6 @@
 {
     'name': "Delivery method mandatory on SO",
-    'version': '9.0.1.0.0',
+    'version': '9.0.1.0.1',
     'depends': ['delivery', 'sale'],
     'author': "Valentin THIRION, AbAKUS it-solutions SARL",
     'website': "http://www.abakusitsolutions.eu",
@@ -9,5 +9,7 @@
 It simply makes the field 'delivery method' mandatory on Sales Order
 
 This module has been developed by Valentin THIRION @ AbAKUS it-solutions""",
-    'data': [],
+    'data': [
+        'views/sale_order_views.xml',
+    ],
 }
--- delivery_mandatory_on_so/models/sale_order.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from openerp import models, fields, api
-
-class sale_order_mandatory_delivery_method(models.Model):
-    _inherit = 'sale.order'
-
-    carrier_id = fields.Many2one("delivery.carrier", string="Delivery Method", help="Fill this field if you plan to invoice the shipping based on picking.", required=True)
